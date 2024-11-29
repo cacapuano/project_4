16a69abd1d53f46164c38578aa7a0b09d3c8aeda
cacapuano <cecapuan@syr.edu>
2024-11-29 11:26:39 -0500

    added some base code for importing file and using fft

--- example.py
+++ example.py
@@ -3,3 +3,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.fft import fft, fftfreq # our project is based around fft
+
+def signal(): # import signal over here
+    data = np.loadtxt() # load data file
+    time = data[]
+    signal = 
+    return time, signal
+
+def use_fft(time, signal):
+    N = len(signal) # length of signal = number of data pts
+    dt = time[1]-time[0] # time interval
+    yf = fft(signal)
+    xf = fftfreq(N, dt)[] # compute frequencies
+
+    # plot signal
